2145784f33709147b30f292ead703486cd6a4a78
Mark Michaels <msquarednet@gmail.com>
2015-02-19 01:19:40 +0000

    add 'display' method to WidgetCurrency prototype

--- test/widget_currency_spec.js
+++ test/widget_currency_spec.js
@@ -1,41 +1,63 @@
-/*globals describe it expect*/
+/*globals describe it expect beforeEach*/
 var WidgetCurrency = function(base,amt) {
     this.base = base || "foo";
     this.amt = amt || 0;
+    // this.display = function() {
+    //     return this.base + " | " + this.amt;
+    // }
 };
+WidgetCurrency.prototype.display = function() {
+        return this.base + " | " + this.amt;
+}
 
 describe("WidgetCurrency", function() {    
     it("exists", function() {
         expect(WidgetCurrency).toBeDefined();
     });
-
+    //var currency = new WidgetCurrency();
+    var currency;
+    beforeEach(function() {
+        currency = new WidgetCurrency();
+    });
     describe("defaults", function() {    
-        it("base is foo", function() {
-            var currency = new WidgetCurrency();
-            //console.log(currency);
+        it("base is foo", function() {            
+            console.log(currency);  //note that 'display' doesnt show up because it is in prototype
             expect(currency.base).toEqual("foo");
+            currency.amt = 100; //to mess with next 'it'
         });
         it ("amt is 0", function() {
-            var currency = new WidgetCurrency();
             expect(currency.amt).toEqual(0);
         });
     });    
 
     describe("setting values with constructor", function() {    
+        var currency;
+        beforeEach(function() {
+            currency = new WidgetCurrency("bar", 10);
+        });
         describe("base", function() {
             it("set base to return bar", function() {
-                var currency = new WidgetCurrency("bar");
                 expect(currency.base).toEqual("bar");
             });
         });
         describe("amt", function() {
             it("set amt to return 10", function() {
-                var currency = new WidgetCurrency(null,10);
                 expect(currency.amt).toEqual(10);
             });
         });
     });    
 
-    
+    describe("display", function() {
+        var currency;
+        beforeEach(function() {
+            currency = new WidgetCurrency("buzz", 100);
+        });
+        describe("base is 'fuzz' and amt is 100", function() {
+            it("is 'buzz' | 10", function() {
+                expect(currency.display()).toEqual('buzz | 100');
+            });
+        });
+    });
     
 });
+// one expectation per 'it'
